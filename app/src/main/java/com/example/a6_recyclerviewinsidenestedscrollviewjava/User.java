9d671270fd3db52fa8b88e9d5e93b5c12a012270
James <sobirovjamshid18@gmail.com>
2022-01-15 14:10:18 +0500

    main

---
+++ app/src/main/java/com/example/a6_recyclerviewinsidenestedscrollviewjava/User.java
@@ -0,0 +1,15 @@
+package com.example.a6_recyclerviewinsidenestedscrollviewjava;
+
+import java.util.ArrayList;
+
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
